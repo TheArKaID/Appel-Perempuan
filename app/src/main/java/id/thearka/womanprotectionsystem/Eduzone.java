f8e8f0b32b077212513f66e37ffecbd0b8b89715
Arifia Kasastra R <arka.progammer@gmail.com>
2019-09-25 10:07:02 +0700

    Create EduZone Fragment

---
+++ app/src/main/java/id/thearka/womanprotectionsystem/Eduzone.java
@@ -0,0 +1,21 @@
+package id.thearka.womanprotectionsystem;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Eduzone extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_eduzone, container, false);
+
+        return v;
+    }
+}
