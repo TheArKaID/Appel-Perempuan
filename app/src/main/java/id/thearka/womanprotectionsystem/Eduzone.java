887128809ca5d4777d35cc7f2328d93c01bad1a7
Arifia Kasastra R <arka.progammer@gmail.com>
2019-09-25 13:30:04 +0700

    Add onCLickListener to Woman's Section

--- app/src/main/java/id/thearka/womanprotectionsystem/Eduzone.java
+++ app/src/main/java/id/thearka/womanprotectionsystem/Eduzone.java
@@ -1,11 +1,10 @@
 package id.thearka.womanprotectionsystem;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,6 +19,7 @@ public class Eduzone extends Fragment {
     private Context context;
 
     private RecyclerView rvWomanEduzone;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -31,8 +31,8 @@ public class Eduzone extends Fragment {
         list = new ArrayList<>();
 
 //        if(savedInstanceState == null){
-            list.addAll(EduzoneData.getListData());
-            showRecyclerList();
+        list.addAll(EduzoneData.getListData());
+        showRecyclerList();
 //            mode = R.id.action_list;
 //        } else{
 //            String setTitle = savedInstanceState.getString(STATE_TITLE);
@@ -46,6 +46,20 @@ public class Eduzone extends Fragment {
         return v;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        ItemClickSupport.addTo(rvWomanEduzone).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemCLicked(RecyclerView mRecyclerView, int adapterPosition, View v) {
+                letWebView(list.get(adapterPosition).getUrl());
+            }
+        });
+    }
+
+    private void letWebView(String url) {
+        String urlWebView = url;
+    }
+
     private void showRecyclerList() {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
         ((LinearLayoutManager) layoutManager).setOrientation(LinearLayoutManager.HORIZONTAL);
@@ -54,11 +68,5 @@ public class Eduzone extends Fragment {
         eduzoneAdapter.setListEduzone(list);
         rvWomanEduzone.setAdapter(eduzoneAdapter);
 
-//        ItemClickSupport.addTo(rvCategory).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
-//            @Override
-//            public void onItemCLicked(RecyclerView mRecyclerView, int adapterPosition, View v) {
-//                showSelectedPresident(list.get(adapterPosition));
-//            }
-//        });
     }
 }
